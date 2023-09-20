7ac7b7e0294bcdc38c90f3ac6de53aca8ce1e787
harigovind <harigovind3020@gmail.com>
2023-09-20 10:58:37 +0530

    first commit

---
+++ users/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+
+
+class Employee(models.Model):
+    DEVELOPER = 'developer'
+    MANAGER = 'manager'
+    USER_ROLES = [
+        (DEVELOPER, 'Developer'),
+        (MANAGER, 'Manager'),
+    ]
+    mobile_number_regex = r'^\d{10}$'
+
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    contact_no = models.IntegerField(max_length=10, validators=[
+        RegexValidator(
+            regex=mobile_number_regex,
+            message="Mobile number must be exactly 10 digits.",
+        ),
+    ])
+    date_of_birth = models.DateField()
+    role = models.CharField(max_length=50, choices=USER_ROLES)
+    department = models.CharField(max_length=50, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Task(models.Model):
+    heading = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField()
+    eta = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    created_by = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True,
+                                   related_name='task_created')
+    assigned_to = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True,
+                                    related_name='task_assigned')
+
+    def clean(self):
+        if self.created_by.role != 'manager' or self.assigned_to.role != 'developer':
+            raise ValidationError("Only managers can assign tasks to developers.")
+
+    def __str__(self):
+        return 'Task - {},'.format(self.heading) + ' Assigned to {}'.format(self.assigned_to.user.username)
---
+++ users/signals.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from .models import Employee, Task
+from django.db.models.signals import post_save,pre_save
+from django.dispatch import receiver
+
+@receiver(post_save,sender=User)
+def createProfile(sender,instance,created,**kwargs):
+    if created:
+        user = instance
+        profile = Employee.objects.create(user=user,)
+        profile.save()
+
+
